7031307e31da01cac1a0976e98aa78577d216835
Andreas Braun <andreas.braun@mongodb.com>
2024-07-25 13:43:18 +0200

    First shot at document class

---
+++ tests/BSON/DocumentTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace MongoDB\Tests\BSON;
+
+use Generator;
+use InvalidArgumentException;
+use MongoDB\BSON\Document;
+use MongoDB\Tests\TestCase;
+
+use function bin2hex;
+use function hex2bin;
+use function pack;
+
+class DocumentTest extends TestCase
+{
+    /** @dataProvider provideInvalidBSONData */
+    public function testFromBSONWithInvalidData(string $data, string $expectedException): void
+    {
+        $this->expectExceptionObject(new InvalidArgumentException($expectedException));
+
+        Document::fromBSON($data);
+    }
+
+    public static function provideInvalidBSONData(): Generator
+    {
+        yield 'Empty string' => [
+            'data' => '',
+            'expectedException' => 'Invalid BSON data',
+        ];
+
+        yield 'Not enough data' => [
+            'data' => pack('Lx', 2),
+            'expectedException' => 'Invalid BSON length',
+        ];
+
+        yield 'Length does not match' => [
+            'data' => pack('Lx', 10),
+            'expectedException' => 'Invalid BSON length',
+        ];
+
+        yield 'Last byte not a null byte' => [
+            'data' => pack('L', 10) . 'abcdef',
+            'expectedException' => 'Invalid BSON length',
+        ];
+    }
+
+    public function testFromBSONValid(): void
+    {
+        $encodedBSON = '0D000000037800050000000000';
+        $document = Document::fromBSON(hex2bin($encodedBSON));
+
+        self::assertSame(hex2bin($encodedBSON), (string) $document);
+    }
+}
